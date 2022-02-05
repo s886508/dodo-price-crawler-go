7c42e43d875c2248cd57fb2e197b98ea933ef3ec
Will WC Huang <s951553@gmail.com>
2022-02-05 15:27:31 +0800

    Support page switch data retriving

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/alexflint/go-arg"
 	"github.com/s886508/dodo-price-crawler-go/pkg/config"
 	"github.com/s886508/dodo-price-crawler-go/pkg/parser"
+	"github.com/s886508/dodo-price-crawler-go/pkg/price"
 )
 
 var args struct {
@@ -17,7 +18,10 @@ var args struct {
 func main() {
 	arg.MustParse(&args)
 	cfg := config.LoadConfig(args.ConfigFile)
-	prices := parser.RetrievePrices(cfg.Url, cfg.Stations)
+	prices := make(price.PriceInfos, 0)
+	for page := 1; page <= 3; page++ {
+		prices = append(prices, parser.RetrievePrices(cfg.Url, page, cfg.Stations)...)
+	}
 
 	bytes, err := json.Marshal(prices)
 	if err != nil {
--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -3,28 +3,33 @@ package parser
 import (
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/s886508/dodo-price-crawler-go/pkg/price"
 )
 
-func RetrievePrices(url string, stations []string) price.PriceInfos {
-	res, err := http.Get(url)
+func RetrievePrices(url_ string, page int, stations []string) price.PriceInfos {
+	v := url.Values{}
+	v.Add("tPage", strconv.Itoa(page))
+	v.Add("Send", "搜尋")
+	res, err := http.PostForm(url_, v)
 	if err != nil {
-		log.Printf("Failed to get response from url: %s, err: %v\n", url, err)
+		log.Printf("Failed to get response from url: %s, err: %v\n", url_, err)
 		return nil
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode < 200 || res.StatusCode > 299 {
-		log.Printf("Http request error, url: %s, status code: %d\n", url, res.StatusCode)
+		log.Printf("Http request error, url: %s, status code: %d\n", url_, res.StatusCode)
 		return nil
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Printf("Failed to create document reader, url: %s, err: %v\n", url, err)
+		log.Printf("Failed to create document reader, url: %s, err: %v\n", url_, err)
 		return nil
 	}
 
@@ -35,7 +40,7 @@ func RetrievePrices(url string, stations []string) price.PriceInfos {
 
 		for _, station := range stations {
 			if strings.Contains(title, station) {
-				tempUrl := url[:strings.LastIndex(url, "/")+1] + link
+				tempUrl := url_[:strings.LastIndex(url_, "/")+1] + link
 				price := retrivePriceDetail(tempUrl, station)
 				prices = append(prices, price)
 			}
@@ -45,22 +50,22 @@ func RetrievePrices(url string, stations []string) price.PriceInfos {
 	return prices
 }
 
-func retrivePriceDetail(url string, station string) *price.PriceInfo {
-	res, err := http.Get(url)
+func retrivePriceDetail(url_ string, station string) *price.PriceInfo {
+	res, err := http.Get(url_)
 	if err != nil {
-		log.Printf("Failed to get response from url: %s, err: %v\n", url, err)
+		log.Printf("Failed to get response from url: %s, err: %v\n", url_, err)
 		return nil
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode < 200 || res.StatusCode > 299 {
-		log.Printf("Http request error, url: %s, status code: %d\n", url, res.StatusCode)
+		log.Printf("Http request error, url: %s, status code: %d\n", url_, res.StatusCode)
 		return nil
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Printf("Failed to create document reader, url: %s, err: %v\n", url, err)
+		log.Printf("Failed to create document reader, url: %s, err: %v\n", url_, err)
 		return nil
 	}
 
